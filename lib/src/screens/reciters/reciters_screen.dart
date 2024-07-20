153baf06949ac89bc035b069e1079c8b46da68d5
Mazen Omar <wildlifemain1@gmail.com>
2024-07-20 17:17:56 +0300

    added policy and home widget test

--- lib/src/screens/reciters/reciters_screen.dart
+++ lib/src/screens/reciters/reciters_screen.dart
@@ -36,67 +36,63 @@ class RecitersScreen extends ConsumerWidget {
           AsyncWidget(
               value: reciters,
               data: (data) {
-                return Expanded(
-                  child: SizedBox(
-                    child: ListView.separated(
-                      itemCount: data.length,
-                      separatorBuilder: (context, index) => const Divider(),
-                      cacheExtent: 50,
-                      itemBuilder: (context, index) {
-                        return Consumer(builder: (context, ref, child) {
-                          return Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: ListTile(
-                              leading: Container(
-                                width: 50,
-                                height: 50,
-                                decoration: BoxDecoration(
-                                    image: DecorationImage(
-                                        fit: BoxFit.cover,
-                                        image: CachedNetworkImageProvider(
-                                            data[index].image!)),
-                                    borderRadius: BorderRadius.circular(12)),
-                              ),
-                              title: Text(data[index].arabicName),
-                              trailing: Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  ToolTipIconButton(
-                                      message: "اختيار الشيخ للتالي",
-                                      onPressed: () {
-                                        ref
-                                            .read(reciterProvider.notifier)
-                                            .state = data[index];
-                                      },
-                                      icon: const Icon(
-                                          Icons.queue_play_next_outlined)),
-                                  const VerticalDivider(),
-                                  ToolTipIconButton(
-                                      message: "اختيار الشيخ",
-                                      onPressed: () {
-                                        final player =
-                                            ref.read(playerSurahProvider);
-
-                                        ref
-                                            .read(reciterProvider.notifier)
-                                            .state = data[index];
-
-                                        ref
-                                            .read(
-                                                playerNotifierProvider.notifier)
-                                            .play(
-                                              surahID: player.surah.id,
-                                            );
-                                      },
-                                      icon: const Icon(Icons.play_arrow)),
-                                ],
-                              ),
+                return SizedBox(
+                  height: MediaQuery.sizeOf(context).height - 285,
+                  child: ListView.separated(
+                    itemCount: data.length,
+                    separatorBuilder: (context, index) => const Divider(),
+                    cacheExtent: 50,
+                    itemBuilder: (context, index) {
+                      return Consumer(builder: (context, ref, child) {
+                        return Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: ListTile(
+                            leading: Container(
+                              width: 50,
+                              height: 50,
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      fit: BoxFit.cover,
+                                      image: CachedNetworkImageProvider(
+                                          data[index].image!)),
+                                  borderRadius: BorderRadius.circular(12)),
                             ),
-                          );
-                        });
-                      },
-                    ),
+                            title: Text(data[index].arabicName),
+                            trailing: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                ToolTipIconButton(
+                                    message: "اختيار الشيخ للتالي",
+                                    onPressed: () {
+                                      ref.read(reciterProvider.notifier).state =
+                                          data[index];
+                                    },
+                                    icon: const Icon(
+                                        Icons.queue_play_next_outlined)),
+                                const VerticalDivider(),
+                                ToolTipIconButton(
+                                    message: "اختيار الشيخ",
+                                    onPressed: () {
+                                      final player =
+                                          ref.read(playerSurahProvider);
+
+                                      ref.read(reciterProvider.notifier).state =
+                                          data[index];
+
+                                      ref
+                                          .read(playerNotifierProvider.notifier)
+                                          .play(
+                                            surahID: player.surah.id,
+                                          );
+                                    },
+                                    icon: const Icon(Icons.play_arrow)),
+                              ],
+                            ),
+                          ),
+                        );
+                      });
+                    },
                   ),
                 );
               }),
---
+++ test/home_test.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mostaqem/src/screens/home/data/surah.dart';
+import 'package:mostaqem/src/screens/home/home_screen.dart';
+import 'package:mostaqem/src/screens/home/providers/home_providers.dart';
+import 'package:mostaqem/src/screens/home/widgets/hijri_date_widget.dart';
+import 'package:mostaqem/src/screens/navigation/data/album.dart';
+import 'package:mostaqem/src/screens/navigation/widgets/player_widget.dart';
+import 'package:mostaqem/src/screens/reciters/data/reciters_data.dart';
+
+void main() {
+  group("Test Home Screen", () {
+    testWidgets('Test Text in Home Screen', (WidgetTester tester) async {
+      await tester.binding.setSurfaceSize(const Size(800, 500));
+
+      await tester.pumpWidget(ProviderScope(overrides: [
+        filterSurahByQueryProvider.overrideWith((ref) => [
+              const Surah(
+                  id: 1,
+                  simpleName: "si",
+                  arabicName: "arabicName",
+                  revelationPlace: "revelationPlace")
+            ]),
+      ], child: MaterialApp(home: HomeScreen())));
+
+      await tester.pump();
+
+      expect(find.text("تاريخ اليوم"), findsOneWidget);
+
+      expect(find.text("arabicName"), findsNWidgets(1));
+
+      expect(find.byType(HijriDateWidget), findsOneWidget);
+    });
+
+    testWidgets('Test Search in Home Screen', (WidgetTester tester) async {
+      await tester.binding.setSurfaceSize(const Size(1280, 780));
+
+      await tester.pumpWidget(ProviderScope(overrides: [
+        fetchAllChaptersProvider.overrideWith((ref) {
+          return [
+            const Surah(
+                id: 0,
+                simpleName: "simpleName",
+                arabicName: "arabicName",
+                revelationPlace: "revelationPlace")
+          ];
+        }),
+      ], child: MaterialApp(home: HomeScreen())));
+
+      await tester.pump();
+
+      // Find Nothing when search for something not found
+      expect(find.text("ماذا تريد ان تسمع؟"), findsOneWidget);
+
+      final searchBar = find.byType(SearchBar);
+
+      await tester.enterText(searchBar, "fff");
+
+      await tester.pumpAndSettle();
+
+      expect(find.text("arabicName"), findsNothing);
+
+      await tester.enterText(searchBar, "arabic");
+
+      await tester.pumpAndSettle();
+
+      expect(find.text("arabicName"), findsOneWidget);
+
+      await tester.enterText(searchBar, "arabicName");
+
+      await tester.pumpAndSettle();
+
+      expect(find.text("arabicName"), findsNWidgets(2));
+    });
+    testWidgets('Test player data in Home Screen', (WidgetTester tester) async {
+      await tester.binding.setSurfaceSize(const Size(800, 500));
+      const surah = Surah(
+          id: 0,
+          simpleName: "simpleName",
+          arabicName: "arabicName",
+          revelationPlace: "revelationPlace");
+
+      const reciter = Reciter(
+          id: 0, englishName: "reciterName", arabicName: "reciterArabicName");
+
+      await tester.pumpWidget(ProviderScope(overrides: [
+        isCollapsedProvider.overrideWith((ref) => true),
+        playerSurahProvider.overrideWith(
+            (ref) => const Album(surah: surah, reciter: reciter, url: "url")),
+      ], child: MaterialApp(home: HomeScreen())));
+
+      await tester.pump();
+    });
+  });
+}
