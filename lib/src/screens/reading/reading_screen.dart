db3a2db764097a8496a8a2d2f15e4467c5ed84c7
MezoPeeta <wildlifemain1@gmail.com>
2025-05-30 14:14:09 +0300

    fixin some things

--- lib/src/screens/reading/reading_screen.dart
+++ lib/src/screens/reading/reading_screen.dart
@@ -159,52 +159,56 @@ class _VerseSpanState extends State<VerseSpan> {
               );
             },
             TextSpan(
-              children: widget.data
-                  .map(
-                    (e) => TextSpan(
-                      text: e.verse.replaceAll('￼', ''),
-                      style: GoogleFonts.amiri(
-                        fontSize: 30,
-                        height: 3.2,
-                        color: Theme.of(context).colorScheme.onSurface,
-                      ),
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () {
-                          context.pushNamed(
-                            'Share',
-                            extra: e.verse.replaceAll('￼', ''),
-                            pathParameters: {
-                              'surahName': widget.surah.id.toString(),
-                            },
-                          );
-                        },
-                      children: [
-                        const WidgetSpan(child: SizedBox(width: 23)),
-                        TextSpan(
+              children:
+                  widget.data
+                      .map(
+                        (e) => TextSpan(
+                          text: e.verse.replaceAll('￼', ''),
+                          style: GoogleFonts.amiri(
+                            fontSize: 30,
+                            height: 3.2,
+                            color: Theme.of(context).colorScheme.onSurface,
+                          ),
+                          recognizer:
+                              TapGestureRecognizer()
+                                ..onTap = () {
+                                  context.pushNamed(
+                                    'Share',
+                                    extra: e.verse.replaceAll('￼', ''),
+                                    pathParameters: {
+                                      'surahName': widget.surah.arabicName,
+                                    },
+                                  );
+                                },
                           children: [
+                            const WidgetSpan(child: SizedBox(width: 23)),
                             TextSpan(
-                              text: e.verseNumber.toString().toArabicNumbers,
+                              children: [
+                                TextSpan(
+                                  text:
+                                      e.verseNumber.toString().toArabicNumbers,
+                                  style: GoogleFonts.amiri(
+                                    color:
+                                        Theme.of(context).colorScheme.primary,
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.w800,
+                                  ),
+                                ),
+                              ],
+                              text: '۝',
                               style: GoogleFonts.amiri(
                                 color: Theme.of(context).colorScheme.primary,
                                 fontSize: 30,
                                 fontWeight: FontWeight.w800,
                               ),
                             ),
+                            const WidgetSpan(
+                              child: SizedBox(width: 15, height: 50),
+                            ),
                           ],
-                          text: '۝',
-                          style: GoogleFonts.amiri(
-                            color: Theme.of(context).colorScheme.primary,
-                            fontSize: 30,
-                            fontWeight: FontWeight.w800,
-                          ),
                         ),
-                        const WidgetSpan(
-                          child: SizedBox(width: 15, height: 50),
-                        ),
-                      ],
-                    ),
-                  )
-                  .toList(),
+                      )
+                      .toList(),
             ),
           );
         },
